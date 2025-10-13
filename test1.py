561d0273bd73154e9842c2f29a0ab1a4970a1628
nguyenngocbaominh <minhm9115@gmail.com>
2025-10-13 20:42:44 +0700

    keep only floyd.py

--- test1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data={
-    "Tên":["Hùng","Nga","Huyền"],
-    "Tuổi":[18,19,21],
-    "Điểm":[8.5,9,9.5]
-}
-df=pd.DataFrame(data)
-print(df)
--- testcsv.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df=pd.read_csv( "D:\Tailieu\programming\student_id,gender,math score,readin.csv")
-print(df)
